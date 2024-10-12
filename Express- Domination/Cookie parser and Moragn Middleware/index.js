53979d52f8b28bedfb0661fbd5a414ac1a4821ec
govindvats21 <govindvats11m@gmail.com>
2024-10-12 23:26:42 +0530

    add

---
+++ Express- Domination/Cookie parser and Moragn Middleware/index.js
@@ -0,0 +1,32 @@
+
+ const express = require('express');
+const app = express();
+
+
+ // Cookie-parser Middleware
+ const cookieParser = require('cookie-parser')
+ app.use(cookieParser())
+
+// Set Cookie Browser
+ app.get("/", (req, res, next)=> {
+ res.cookie("banned", "true")
+ res.send("banned")
+ })
+
+ // read cookie in other route
+ app.get("/check", (req, res, next)=> {
+ console.log(req.cookies.banned);
+ res.send("check")
+ })
+ 
+
+ // Morgan Middleware
+ const morgan = require('morgan')
+
+ app.use(morgan("common"))
+
+ app.get("/morgan", (req, res, next)=> {
+ res.send("hello")
+ })
+
+app.listen(3000)
